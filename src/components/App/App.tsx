b3d9b6cf8e790ec65a14492adbedf12174421fdb
juliya.lysenko <juliya.deleh@gmail.com>
2025-07-14 16:21:21 +0300

    commit3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,28 +5,41 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
 import type { Movie } from "../../types/movie";
-import fetchMovies 
+
+import { fetchMovies } from "../../services/movieService";
 
 import css from "./App.module.css";
 
 import { Toaster } from "react-hot-toast";
 
 export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-    const [movies, setMovies] = useState<Movie[]>([]);
+  const handleSubmit = async (topic: string) => {
+    try {
+      setIsLoading(true);
 
-
-    const handleSubmit = async (query: string) => void;
-    
-    const response = await fetchMovies(query);
+      setIsError(false);
+      const data = await fetchMovies(topic);
+      setMovies(data);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />;
-      <SearchBar onSubmit={handleSubmit} />;
-      {/* {loading && <Loader />}
-      {error && <ErrorMessage />} */}
-      {movies.length > 0 && <MovieGrid movies={movies} onSelect={setSelectedMovie} />}
+      <SearchBar onSubmit={handleSubmit} />;{isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={setSelectedMovie} />
+      )}
     </>
   );
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,1 +1,35 @@
-fetchMovies;
+// import axios from "axios";
+// import type { Movie } from "../types/movie";
+
+// interface MoviesHttpResponse {
+//   hits: Movie[];
+// }
+
+// // HTTP-функція запиту статей
+// export const fetchMovies = async (topic: string): Promise<Movie[]> => {
+//   const response = await axios.get<MoviesHttpResponse>(
+//     `https://hn.algolia.com/api/v1/search?query=${topic}`
+//   );
+//   return response.data.hits;
+// };
+
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+
+export async function fetchMovies(query: string): Promise<Movie[]> {
+  const token = import.meta.env.VITE_TMDB_TOKEN;
+
+  const config = {
+    params: {
+      query,
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const { data } = await axios.get(BASE_URL, config);
+  return data.results;
+}
